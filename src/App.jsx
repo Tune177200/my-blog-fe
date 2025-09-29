595ed0040e8299ad1eb476ba26f799fc43e8add1
tunv1 <tunv1@newwave.vn>
2025-09-29 10:30:16 +0700

    Register Loguin

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import MyBlog from "./pages/MyBlog"
 import Register from "./pages/Register"
+import Login from "./pages/Login"
 
 function App() {
   return (
@@ -10,12 +11,14 @@ function App() {
         <Link to="/">Home</Link> |{" "}
         <Link to="/my-blog">My Blog</Link> |{" "}
         <Link to="/register">Register</Link> |{" "}
+        <Link to="/login">Login</Link> |{" "}
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/my-blog" element={<MyBlog />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   )
